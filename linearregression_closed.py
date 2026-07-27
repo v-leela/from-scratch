37997bb0b490edeb12bbafe7ffcf4083e3651db8
blank n white crow <valluri4496@gmail.com>
2026-07-27 16:29:06 +0530

    updated lr closed

--- linearregression_closed.py
+++ linearregression_closed.py
@@ -1,20 +1,35 @@
 import numpy as np
-""" 
-class LinearRegressionClosed:
 
+class LinearRegressionClosed:
     def __init__(self):
         self.coef = None
         self.intercept = 0
+        self.theta = None
 
     def fit(self, X, y):
         X = np.array(X)
         y = np.array(y)
+        self.X = X
+        self.y = y
 
         Xb = np.c_[np.ones((X.shape[0], 1)), X]
 
-    def predict(self, X):
-        pass """
+        # theta from solving the gradient of the jacobian
+        theta = np.linalg.inv(Xb.T @ Xb) @ Xb.T @ y
+        
+        self.coef = theta[1:,]
+        self.intercept = theta[0,]
+        self.theta = theta
 
-x = np.array([1,23,4,5])
-y = np.ones((4,1))
-print(np.c_[y,x])
+    def predict(self, X):
+        X = np.array(X)
+        Xb = np.c_[np.ones((X.shape[0], 1)), X]
+
+        if self.theta is not None:
+            return Xb @ self.theta
+    
+    def score(self, X_test, y_test):
+        y_pred = self.predict(X_test)
+        r_sq = 1 - np.sum((y_test - y_pred) ** 2) / np.sum((y_test - np.mean(y_test)) ** 2)
+
+        return r_sq
